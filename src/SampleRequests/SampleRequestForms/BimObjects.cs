46472b3e460246844b1cf860056dd91cd9876d08
azarakhshr <azarakhshr@AZARAKHSH-PC>
2019-03-05 17:11:12 +0100

    initial code

---
+++ src/SampleRequests/SampleRequestForms/BimObjects.cs
@@ -0,0 +1,65 @@
+﻿namespace SampleRequestForms
+{
+
+
+    public class BimResponse
+    {
+        public Response response { get; set; }
+    }
+
+    public class Response
+    {
+        public Result[] result { get; set; }
+    }
+
+    public class Result
+    {
+        public string __type { get; set; }
+        public int checkinInProgress { get; set; }
+        public object[] checkouts { get; set; }
+        public int[] concreteRevisions { get; set; }
+        public int createdById { get; set; }
+        public long createdDate { get; set; }
+        public string description { get; set; }
+        public string exportLengthMeasurePrefix { get; set; }
+        public int[] extendedData { get; set; }
+        public int geoTagId { get; set; }
+        public int[] hasAuthorizedUsers { get; set; }
+        public int id { get; set; }
+        public int lastConcreteRevisionId { get; set; }
+        public int lastRevisionId { get; set; }
+        public int[] logs { get; set; }
+        public int[] modelCheckers { get; set; }
+        public string name { get; set; }
+        public object[] newServices { get; set; }
+        public int oid { get; set; }
+        public int parentId { get; set; }
+        public int[] revisions { get; set; }
+        public int rid { get; set; }
+        public string schema { get; set; }
+        public bool sendEmailOnNewRevision { get; set; }
+        public object[] services { get; set; }
+        public string state { get; set; }
+        public object[] subProjects { get; set; }
+        public string uuid { get; set; }
+    }
+
+
+    public class BimRequest
+    {
+        public string token { get; set; }
+        public Request request { get; set; }
+    }
+
+    public class Request
+    {
+        public string @interface { get; set; }
+        public string method { get; set; }
+        public Parameters parameters { get; set; }
+    }
+
+    public class Parameters
+    {
+        public string name { get; set; }
+    }
+}
---
+++ src/SampleRequests/SampleRequestForms/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace SampleRequestForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var httpClient = new HttpClient();
+            var body = new BimRequest();
+            body.token = "50cefff41e589d0d6eb525006ece49d1c998b221f027d5faa58eb8f0126baea5e672d66656f077f44302590aedc630b7";
+            var par = new Parameters() { name = "Connect020_V3" };
+            var request = new Request() { @interface = "ServiceInterface", method = "getProjectsByName", parameters = par };
+            body.request = request;
+            var json = JsonConvert.SerializeObject(body);
+            var s = new StringContent(json);
+
+            var result = await httpClient.PostAsync("http://localhost:8082/json", s);
+            string resp = result.Content.ReadAsStringAsync().Result;
+            var bimResponse = JsonConvert.DeserializeObject<BimResponse>(resp);
+            button1.Text = bimResponse.response.result[0].__type;
+
+        }
+    }
+}
